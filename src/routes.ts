26f8e25ccb6e3c9da37415dff78c0cc007719a6a
OWK50GA <okoriewifrid.20201206203@futo.edu.ng>
2026-06-01 08:13:01 +0100

    feat(routes): add insert request and validation to POST /request

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,14 @@
 import { Request, Response, Router } from "express";
 import { isValidHttpMethod, VALID_METHODS } from "./utils";
+import db from "./db";
+import { v7 } from 'uuid';
 
 const router = Router();
+
+const insertRequest = db.prepare(`
+    INSERT INTO requests (id, url, method, body max_retries, backoff_ms)
+    VALUES (@id, @url, @method @body, @maxRetries, @backoffMs)
+`);
 
 router.post("/request", (req: Request, res: Response) => {
   const { url, method, body, maxRetries, backoffMs } = req.body;
@@ -32,6 +39,46 @@ router.post("/request", (req: Request, res: Response) => {
       status: "error",
       message: `Invalid method. Must be one of: ${VALID_METHODS.join(", ")}`,
     });
+  }
+
+  if (maxRetries) {
+    if (isNaN(parseInt(maxRetries))) {
+        return res.status(400).json({
+            status: "error",
+            message: "maxRetries must be a number if present"
+        });
+    }
+  }
+
+  if (backoffMs) {
+    if (isNaN(parseInt(backoffMs))) {
+        return res.status(400).json({
+            status: "error",
+            message: "backoffMs must be a millisecond number if present",
+        });
+    }
+  }
+
+  const id = v7();
+  
+  try {
+    insertRequest.run({
+        id,
+        url,
+        method,
+        body: body ? JSON.stringify(body) : null,
+        maxRetries,
+        backoffMs
+    })
+    return res.status(201).json({
+        id,
+        status: "pending",
+    })
+  } catch (err) {
+    return res.status(500).json({
+        status: "error",
+        message: "Failed to save request"
+    })
   }
 });
 
